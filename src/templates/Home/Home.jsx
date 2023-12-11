6ff7e950ae5ea633c5979c6276c33a2ab52203a0
Schubert Araujo <xubsoldier@gmail.com>
2023-12-11 12:14:08 -0300

    Alteração do titulo

--- src/templates/Home/Home.jsx
+++ src/templates/Home/Home.jsx
@@ -4,8 +4,17 @@ import { PhotoProfile } from '../../components/profileCard/PhotoProfile';
 import { ProfileInfoBot } from '../../components/profileCard/CardProfileBottom';
 import { ProfileInfoTop } from '../../components/profileCard/CardProfileTop';
 import './style.css';
+import { useEffect } from 'react';
 
 export function Home() {
+  useEffect(() => {
+    document.title = 'Perfil Pessoal';
+
+    return () => {
+      document.title = 'React App';
+    };
+  }, []);
+
   return (
     <div>
       <div className="conteiner profile-card">
